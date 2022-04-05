ce026dce0440971054f2a929109b496289db5cce
Tomasz Bączek <tb054379@student.ath.edu.pl>
2022-04-05 16:48:14 +0200

    form update

--- Models/UserInputModel.cs
+++ Models/UserInputModel.cs
@@ -7,9 +7,21 @@ namespace PracaInżynierskaTomaszBaczek.Models
 {
     public class UserInputModel
     {
-        public string HillName { get; set; }
-        public string CountryCode { get; set; } 
+        public string HillName { get; set; } = "Default";
+        public string CountryCode { get; set; } = "POL";
         public int HillSize { get; set; } = 140;
+        public string JudgetowerColorOut { get; set; } = "0x959183";
+        public string JudgetowerColorIn { get; set; } = "0x78554e";
+        public string JudgetowerTextureOut { get; set; } = @"Textures\wood3.png";
+        public string JudgetowerTexstureIn { get; set; } = @"Textures\wood3.png";
+        public string PillarTexture { get; set; } = @"Textures\railing-glass.png";
+        public string PillarColor { get; set; } = "0xf2e5c7";
+        public string RailingTexture { get; set; } = @"Textures\railing-glass.png";
+        public string RailingColor { get; set; } = "0xf2e5c7";
+        public string InrunColor { get; set; } = "0x97a594";
+        public string PlasticColor { get; set; } = "0x769391";
+        public string TerrainColor1 { get; set; } = "0x443920";
+        public string TerrainColor2 { get; set; } = "0x5D603A";
 
-        }
+    }
 }
